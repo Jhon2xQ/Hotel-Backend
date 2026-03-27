72a3fea0b6f62e73dbc901d5d64941eabee744fa
Jhon2xQ <jhonq2512@gmail.com>
2026-03-27 13:42:24 -0500

    fix:casos de uso corregido

--- src/application/use-cases/reserva/create-reserva.use-case.ts
+++ src/application/use-cases/reserva/create-reserva.use-case.ts
@@ -1,11 +1,29 @@
 import { IReservaRepository } from "../../../domain/interfaces/reserva.repository.interface";
 import { Reserva } from "../../../domain/entities/reserva.entity";
 import { CreateReservaInput } from "../../dtos/reserva.dto";
+import { ReservaException } from "../../../domain/exceptions/reserva.exception";
 
 export class CreateReservaUseCase {
   constructor(private reservaRepository: IReservaRepository) {}
 
   async execute(input: CreateReservaInput): Promise<Reserva> {
+    // Validaciones de negocio
+    if (input.fechaSalida <= input.fechaEntrada) {
+      throw ReservaException.invalidDateRange();
+    }
+    if (input.adultos < 1) {
+      throw ReservaException.invalidAdultos();
+    }
+    if (input.ninos < 0) {
+      throw ReservaException.invalidNinos();
+    }
+
+    // Verificar código único
+    const existingCodigo = await this.reservaRepository.findByCodigo(input.codigo);
+    if (existingCodigo) {
+      throw ReservaException.duplicateCodigo(input.codigo);
+    }
+
     return await this.reservaRepository.create({
       codigo: input.codigo,
       huespedId: input.huespedId,
--- src/application/use-cases/reserva/update-reserva.use-case.ts
+++ src/application/use-cases/reserva/update-reserva.use-case.ts
@@ -1,11 +1,36 @@
 import { IReservaRepository } from "../../../domain/interfaces/reserva.repository.interface";
 import { Reserva } from "../../../domain/entities/reserva.entity";
 import { UpdateReservaInput } from "../../dtos/reserva.dto";
+import { ReservaException } from "../../../domain/exceptions/reserva.exception";
 
 export class UpdateReservaUseCase {
   constructor(private reservaRepository: IReservaRepository) {}
 
   async execute(id: string, input: UpdateReservaInput): Promise<Reserva> {
+    // Verificar que la reserva existe
+    const existing = await this.reservaRepository.findById(id);
+    if (!existing) {
+      throw ReservaException.notFoundById();
+    }
+
+    // Verificar si está completada
+    if (existing.estado === "COMPLETADA") {
+      throw ReservaException.cannotModifyCompleted();
+    }
+
+    // Validaciones de negocio
+    if (input.fechaEntrada && input.fechaSalida) {
+      if (input.fechaSalida <= input.fechaEntrada) {
+        throw ReservaException.invalidDateRange();
+      }
+    }
+    if (input.adultos !== undefined && input.adultos < 1) {
+      throw ReservaException.invalidAdultos();
+    }
+    if (input.ninos !== undefined && input.ninos < 0) {
+      throw ReservaException.invalidNinos();
+    }
+
     return await this.reservaRepository.update(id, input);
   }
 }
