782e93892939954311b53d850612b2a1ca3f1b0d
Jhon Alberth Quispe Quispe <157059820+Jhon2xQ@users.noreply.github.com>
2026-03-29 14:29:27 -0500

    fix:lista y obtener por id añadido a ruta privada (#37)

--- src/routes/tipo-habitacion.routes.ts
+++ src/routes/tipo-habitacion.routes.ts
@@ -9,6 +9,8 @@ export function createTipoHabitacionRoutes(): AppHono {
   const ctrl = container.resolve(TipoHabitacionController);
   const router = new Hono<{ Variables: AppVariables }>();
 
+  router.get("/", (c) => ctrl.list(c));
+  router.get("/:id", validParams(UUIDParamSchema), (c) => ctrl.findById(c));
   router.post("/", validSchema(CreateTipoHabitacionSchema), (c) => ctrl.create(c));
   router.put("/:id", validParams(UUIDParamSchema), validSchema(UpdateTipoHabitacionSchema), (c) => ctrl.update(c));
   router.delete("/:id", validParams(UUIDParamSchema), (c) => ctrl.delete(c));
